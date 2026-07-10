5cbebb55bade4870aa7e86e98b4bf84796e12782
endy7540 <ldg7711@gmail.com>
2026-07-10 11:36:50 +0900

    adf

--- src/main/java/com/example/planslot/PlanSlotApplication.java
+++ src/main/java/com/example/planslot/PlanSlotApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PlanSlotApplication {
 
+    // dkssuadadks
     public static void main(String[] args) {
         SpringApplication.run(PlanSlotApplication.class, args);
     }

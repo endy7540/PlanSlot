21af91709e3c5993d2599d47d559c75270ecfb61
kjyq1 <zemdn0915@gmail.com>
2026-07-10 12:04:01 +0900

    test2

--- src/main/java/com/example/planslot/PlanSlotApplication.java
+++ src/main/java/com/example/planslot/PlanSlotApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PlanSlotApplication {
 
-    //
+    //asdf
     public static void main(String[] args) {
         SpringApplication.run(PlanSlotApplication.class, args);
     }

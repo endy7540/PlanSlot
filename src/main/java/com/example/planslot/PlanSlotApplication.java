9dd26806c6409a9c182bad7cd82f1103ddcca218
jeon-jeongsu <dhb0383@gmail.com>
2026-07-10 12:30:48 +0900

    test4

--- src/main/java/com/example/planslot/PlanSlotApplication.java
+++ src/main/java/com/example/planslot/PlanSlotApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PlanSlotApplication {
 
-    // D
+    // Div
     public static void main(String[] args) {
         SpringApplication.run(PlanSlotApplication.class, args);
     }

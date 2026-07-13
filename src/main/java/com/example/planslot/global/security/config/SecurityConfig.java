a340e32c7b7d2165b41c2a9fdca92787a4bac2d8
pigugong102 <a56455946@gmail.com>
2026-07-13 15:23:54 +0900

    403 에러 수정

--- src/main/java/com/example/planslot/global/security/config/SecurityConfig.java
+++ src/main/java/com/example/planslot/global/security/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/api/members/signup", "/api/auth/login", "/error", "/auth", "/home").permitAll()
+                        .requestMatchers("/", "/api/members/signup", "/api/auth/login", "/error", "/auth", "/home", "/favicon.ico").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);

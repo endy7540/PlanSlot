83b9b7baa0ac56b13329328f5ce6e645d2ee1ca0
pigugong102 <a56455946@gmail.com>
2026-07-20 09:19:51 +0900

    에러 수정

--- src/main/java/com/example/planslot/member/servcie/MemberServiceImpl.java
+++ src/main/java/com/example/planslot/member/servcie/MemberServiceImpl.java
@@ -35,8 +35,6 @@ public class MemberServiceImpl implements MemberService{
                 .address(request.getAddress())
                 .role(Member.Role.MEMBER)
                 .status(Member.Status.ACTIVE)
-                .allowActivityNoti(request.isAllowActivityNoti())
-                .allowMarketingNoti(request.isAllowMarketingNoti())
                 .build();
 
         return memberRepository.save(member).getId();
@@ -56,8 +54,6 @@ public class MemberServiceImpl implements MemberService{
                 .nickname(member.getNickname())
                 .address(member.getAddress())
                 .profileImageUrl(member.getProfileImageUrl())
-                .allowActivityNoti(member.isAllowActivityNoti())
-                .allowMarketingNoti(member.isAllowMarketingNoti())
                 .role(member.getRole().name())
                 .build();
     }
